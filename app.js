369195fd250158a2b132e8f0eb1c10d4de04bdab
dewanshpratapsingh <dewansh.mca21.du@gmail.com>
2025-07-01 13:31:25 +0000

    changed cors logic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
-    origin: 'https://your-frontend.vercel.app',
+    origin: 'https://financial-assist-git-development-dewanshpratapsinghs-projects.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 };
