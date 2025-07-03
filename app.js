15cfbd28c0c9903dc3ab61ba580bdfd6449f86a6
Dewansh Pratap Singh <42464682+dewanshpratapsingh@users.noreply.github.com>
2025-07-03 19:35:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const corsOptions = {
     origin: 'https://financial-assist.vercel.app/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
 /** To Allow some clients to access this app resources */
 app.use(cors(corsOptions));
